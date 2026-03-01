bb57a62a6bca6d42e27d4190f42b1e659b5341ac
sabilafirdias <sabilafirdias@gmail.com>
2026-03-01 23:00:22 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-gray-800 text-white shadow-sm sm:rounded-lg p-6">
+                <div class="space-y-2 text-lg">
+                    <p>Nama : Sabila Firdiastuti Maulani Supratman</p>
+                    <p>NIM : 20230140192</p>
+                    <p>Program Studi : Teknologi Informasi</p>
+                    <p>Hobi : Nonton</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
